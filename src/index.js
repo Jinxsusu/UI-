6b64b89e42bf8d36ba92a1caa22bcaa35c860c92
Jinx <Jinx@qq.com>
2019-11-29 09:11:59 +0800

    首页静态页面

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'antd-mobile/dist/antd-mobile.css';  // or 'antd-mobile/dist/antd-mobile.less'
+import 'lib-flexible';
+import 'antd-mobile/dist/antd-mobile.css';
+import './index.css'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react'
 import './index.css'
-import { Card} from 'antd-mobile'
 export default class Home extends Component {
     state = {
-        city:'北京'
+        carname: "京Q392U9",
+        mobile: '18510637932',
+        brandList: [
+            { id: 1, name: '大地车险', select: true },
+            { id: 2, name: '人保财险', select: true },
+            { id: 3, name: '太平洋车险', select: false },
+            { id: 4, name: '阳光车险', select: false }
+        ]
     }
     render() {
         return (
-            <div className="home">
-                <div className="header">
-                <Card full>
-                    <Card.Body>
-                        <div>车险</div>
-                    </Card.Body>
-                        <Card.Footer content="行驶车辆&#58;" extra={<span>{this.state.city}</span>} />
-                    </Card>
+            <div className='home clearfix'>
+                <div className='header'>
+                    <div className="title">
+                        车险
+                </div>
+                    <div className="body">
+                        <span>行驶车辆&#58;
+                    </span>
+                        <span>
+                            北京
+                    </span>
+                    </div>
+                </div>
+                {/* 车辆信息 */}
+                <div className="carInfo">
+                    <p>
+                        <span>车牌</span>
+                        <span>{this.state.carname}</span>
+                    </p>
+                    <p>
+                        <span>手机号</span>
+                        <span>{this.state.mobile}</span>
+                    </p>
+                </div>
+                {/* 保险品牌 */}
+                <div className="brand">
+                    <p className="heading">
+                    </p>
+                    <div className="content">
+                        {this.state.brandList.map((item, index) => {
+                            return <div className="item" key={item.id}>
+                                <img className="hot" src={require('../../assets/images/hot .png')} alt='' />
+                                <img className="logo" src={require('../../assets/images/logo_1.png')} alt="" />
+                                <p className="brandName">{item.name}</p>
+                                <img className="success" src={item.select ? require('../../assets/images/success.png') : require('../../assets/images/false.png')} alt="" />
+                            </div>
+                        })}
+                    </div>
+                </div>
+                <div className="agreement">
+                    请务必阅读&nbsp;&nbsp;<span>《平台服务协议》&nbsp;&nbsp;《服务告知书》&nbsp;&nbsp;《车险报价服务题型说明》</span>
+                </div>
+                <div className="btn">
+                    <p>一键报价</p>
                 </div>
             </div>
         )
